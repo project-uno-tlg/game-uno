763036c7aae8a6f610a423085fc0bd0e76ea4009
PengChen11 <peng.chen11@outlook.com>
2021-06-22 16:35:42 -0700

    better game logic

--- test/com/unomas/dealer/DealerBotTest.java
+++ test/com/unomas/dealer/DealerBotTest.java
@@ -1,8 +1,8 @@
 package com.unomas.dealer;
 
+import com.unomas.player.Player;
 import org.junit.Before;
 import org.junit.Test;
-import com.unomas.dealer.DealerBot.Player;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,24 +14,27 @@ public class DealerBotTest {
 
     @Before
     public void init(){
-        players = Arrays.asList(
-                new Player("testA", Arrays.asList(
-                        Card.getInstance(Card.CardColor.BLUE, 9),
-                        Card.getInstance(Card.CardColor.YELLOW, 7)
-                )),
-                new Player("testB", Arrays.asList(
-                        Card.getInstance(Card.CardColor.BLUE, 9),
-                        Card.getInstance(Card.CardColor.YELLOW, 6)
-                )),
-                new Player("testC", Arrays.asList(
-                        Card.getInstance(Card.CardColor.RED, 9),
-                        Card.getInstance(Card.CardColor.YELLOW, 7)
-                )));
-        dealerBot = DealerBot.getInstance(players);
+//        players = Arrays.asList(
+//                new Player("testA", Arrays.asList(
+//                        Card.getInstance(Card.CardColor.BLUE, 9),
+//                        Card.getInstance(Card.CardColor.YELLOW, 7)
+//                )),
+//                new Player("testB", Arrays.asList(
+//                        Card.getInstance(Card.CardColor.BLUE, 9),
+//                        Card.getInstance(Card.CardColor.YELLOW, 6)
+//                )),
+//                new Player("testC", Arrays.asList(
+//                        Card.getInstance(Card.CardColor.RED, 9),
+//                        Card.getInstance(Card.CardColor.YELLOW, 7)
+//                )));
+//        dealerBot = DealerBot.getInstance(players);
     }
 
     @Test
     public void randomPlayer_returnsCorrectNumberInRange(){
+        for (int i =0; i<20; i++){
+            System.out.println(DealerBot.randomPlayer(5));
+        }
 
     }
 }
